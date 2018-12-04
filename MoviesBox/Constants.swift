be80095afc528dfe7db95bace3d143306a676d5a
Priyanka <virupakshfutane@virupakshs-macbook-pro.local>
2018-12-04 02:59:00 -0500

    Added Constants file

---
+++ MoviesBox/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  MoviesBox
+//
+//  Created by Priyanka Saroha on 12/1/18.
+//  Copyright © 2018 Priyanka Saroha. All rights reserved.
+//
+
+import Foundation
+
+let API_KEY = "a166bf64c10195201e231dddd2dc7f0e"
+let MOVIE_LIST_URL = "https://api.themoviedb.org/3/movie/%@?api_key=%@"
+let BASE_MOVIE_POSTER_URL = "https://image.tmdb.org/t/p/w500"
+
+let NOWPLAYING_FETCH_REQUEST = "NowPlayingMovies"
+let TOPRATED_FETCH_REQUEST = "TopRatedMovies"
+
+let NO_NETWORK_ERROR = "Please check connectivity to network."
+let MOVIE_FETCH_FAIL_ALERT_TITLE = "Error"
